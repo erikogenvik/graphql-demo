85809509d92488d6c6776871606a3e1f2a645e4b
Erik Ogenvik <erik@ogenvik.org>
2019-05-23 14:26:35 +0200

    Simplify

--- backend/src/main/java/com/jayway/kdag/graphql/Persons.kt
+++ backend/src/main/java/com/jayway/kdag/graphql/Persons.kt
@@ -1,6 +1,5 @@
 package com.jayway.kdag.graphql
 
-import com.couchbase.lite.CouchbaseLiteException
 import com.couchbase.lite.Document
 import org.reactivestreams.Publisher
 import org.slf4j.LoggerFactory
@@ -8,55 +7,34 @@ import org.springframework.stereotype.Component
 import reactor.core.publisher.DirectProcessor
 import reactor.core.publisher.FluxSink
 import java.lang.invoke.MethodHandles
-import java.util.*
 
 @Component
 class Persons internal constructor(private val dataStore: DataStore) : PersonMutation, PersonQuery {
 
     val sinks = mutableListOf<FluxSink<Person>>()
 
-    override fun createPerson(firstName: String, lastName: String): Person? {
-        return try {
-            // Create a new document (i.e. a record) in the database.
-            val document = dataStore.database.createDocument()
-            val properties = HashMap<String, Any>()
-            properties["firstName"] = firstName
-            properties["lastName"] = lastName
-            document.putProperties(properties)
+    override fun createPerson(firstName: String, lastName: String): Person {
+        val document = dataStore.database.createDocument().putProperties(mapOf("firstName" to firstName, "lastName" to lastName))
 
-            val person = Person(document.id, firstName, lastName)
-            sinks.forEach { it.next(person) }
-            person
-        } catch (e: CouchbaseLiteException) {
-            log.error("Error when saving to database.", e)
-            null
-        }
+        val person = Person(document.id, firstName, lastName)
+        sinks.forEach { it.next(person) }
+        return person
 
     }
 
     override fun deletePerson(id: String): Boolean {
-        try {
-            val existingDocument = dataStore.database.getExistingDocument(id)
-            if (existingDocument != null) {
-                return existingDocument.delete()
-            }
-        } catch (e: CouchbaseLiteException) {
-            log.error("Error when deleting from database.", e)
+        val existingDocument = dataStore.database.getExistingDocument(id)
+        if (existingDocument != null) {
+            return existingDocument.delete()
         }
 
         return false
     }
 
     override fun people(): List<Person>? {
-        return try {
-            val result = dataStore.database.createAllDocumentsQuery().run()
+        val result = dataStore.database.createAllDocumentsQuery().run()
 
-            result.map { queryRow -> toPerson(queryRow.document) }
-        } catch (e: CouchbaseLiteException) {
-            log.error("Error when getting from database.", e)
-            null
-        }
-
+        return result.map { queryRow -> toPerson(queryRow.document) }
     }
 
     fun subscribeNew(): Publisher<Person> {
